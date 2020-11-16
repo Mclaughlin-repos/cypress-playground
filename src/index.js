d0df91d7c54600556af319035e42d1b43cce0ee1
mclaughlin <mclaughlin@indeed.com>
2020-11-16 14:17:09 -0600

    setup-webpack set up webpack configs for enhanced working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import 'dotenv/config';
-console.log('hello Node.js project');
-console.log(process.env.ACCESS_TOKEN_FOR_APP);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+const title = 'React with Webpack and Babel';
+ 
+ReactDOM.render(
+  <App title={title}/>,
+  document.getElementById('app')
+);
+module.hot.accept();
