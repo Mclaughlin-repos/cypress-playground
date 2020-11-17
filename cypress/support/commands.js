11ada027a6b28b958cda1f0c5777767f350cbc0a
mclaughlin <mclaughlin@indeed.com>
2020-11-17 13:21:42 -0600

    axe-install installed axe-core, cypress-axe, modified cypress-axe to use correct package with support/commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,7 +7,15 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
+// -- This overwrites the axe-core package to use the axe.min because the is a known issue with using the package out of the box with internal axe-core being broken so need to target axe.min --
+Cypress.Commands.add('addAxeCode', () => {
+  cy.window({ log: false }).then(window => {
+      const axe = require('axe-core/axe.js');
+      const script = window.document.createElement('script');
+      script.innerHTML = axe.source;
+      window.document.head.appendChild(script);
+  });
+});
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
@@ -23,3 +31,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
