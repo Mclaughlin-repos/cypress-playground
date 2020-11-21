ac3500f3f6315b32bd6c0117c380418b915cd1ea
mclaughlin <mclaughlin@indeed.com>
2020-11-21 13:27:08 -0600

    add-eslint-prettier added eslint and prettier with simple base configs

--- src/components/LabelContainer/index.tsx
+++ src/components/LabelContainer/index.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LabelContainer: React.FC<{
-  className?: string;
-  id: string;
-  children?: React.ReactNode;
-  dataCy: string;
-  htmlFor: string;
+    className?: string;
+    id: string;
+    children?: React.ReactNode;
+    dataCy: string;
+    htmlFor: string;
 }> = ({ id, dataCy, children, className, htmlFor }) => {
-  return (
-      <Label id={id} className={className} htmlFor={htmlFor} data-cy={dataCy}>
-          {children}
-      </Label>
-  );
+    return (
+        <Label id={id} className={className} htmlFor={htmlFor} data-cy={dataCy}>
+            {children}
+        </Label>
+    );
 };
 
 const Label = styled.label`
-  border: 1px solid red;
-  font-size: 1rem;
+    border: 1px solid red;
+    font-size: 1rem;
 `;
 
 export default LabelContainer;
